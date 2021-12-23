9bf7b0fa15501d992896f000bf1dee5c3fd3d912
Nikita <medvedhopod@gmail.com>
2021-12-23 14:19:21 +0200

    TicTacToe 4 stage

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -2,67 +2,128 @@ import java.util.Scanner;
 
 public class TicTacToe {
     Scanner userInput = new Scanner(System.in);
-    String field = "";
+    String line1 = "___";
+    String line2 = "___";
+    String line3 = "___";
+    char moveNow = 'X';
 
     public static void main(String[] args) {
         TicTacToe myGame = new TicTacToe();
         myGame.setField();
         myGame.showField();
-        System.out.println(myGame.checkGameStatus());
+        myGame.makeMove();
+        myGame.showField();
     }
+
     public int countMatches(String target) {
-        String deleteTarget = field.replace (target, "");
-        return field.length () - deleteTarget.length ();
+        String deleteTarget1 = line1.replace (target, "");
+        String deleteTarget2 = line2.replace (target, "");
+        String deleteTarget3 = line3.replace (target, "");
+        return (line1.length () + line2.length() + line3.length()) - (deleteTarget1.length () + deleteTarget2.length()
+                + deleteTarget3.length());
     }
-    public void setField
-            () {
+    public void setField() {
         System.out.println("Enter cells:");
         String newField = userInput.nextLine();
-        field = newField;
+        line1 = newField.substring(0,3);
+        line2 = newField.substring(3, 6);
+        line3 = newField.substring(6, 9);
     }
     public void showField() {
         System.out.printf("---------\n" +
                 "| %s |\n" +
                 "| %s |\n" +
                 "| %s |\n" +
-                "---------", field.substring(0, 3), field.substring(3, 6), field.substring(6, 9));
+        "---------\n", line1, line2, line3);
     }
     public String checkGameStatus() {
         boolean oWin = false;
         boolean xWin = false;
-
-        if (field.charAt(0) == 'O' && field.charAt(1) == 'O' && field.charAt(2) == 'O' || field.charAt(3) == 'O' &&
-                field.charAt(4) == 'O' && field.charAt(5) == 'O' || field.charAt(6) == 'O' && field.charAt(7) == 'O' &&
-                field.charAt(8) == 'O' || field.charAt(0) == 'O' && field.charAt(3) == 'O' && field.charAt(6) == 'O' ||
-                field.charAt(1) == 'O' && field.charAt(4) == 'O' && field.charAt(7) == 'O' || field.charAt(2) == 'O' &&
-                field.charAt(5) == 'O' && field.charAt(8) == 'O' || field.charAt(0) == 'O' && field.charAt(4) == 'O' &&
-                field.charAt(8) == 'O' || field.charAt(2) == 'O' && field.charAt(4) == 'O' && field.charAt(6) == 'O') {
-            oWin = true;
-        }
-
-        if (field.charAt(0) == 'X' && field.charAt(1) == 'X' && field.charAt(2) == 'X' || field.charAt(3) == 'X' &&
-                field.charAt(4) == 'X' && field.charAt(5) == 'X' || field.charAt(6) == 'X' && field.charAt(7) == 'X' &&
-                field.charAt(8) == 'X' || field.charAt(0) == 'X' && field.charAt(3) == 'X' && field.charAt(6) == 'X' ||
-                field.charAt(1) == 'X' && field.charAt(4) == 'X' && field.charAt(7) == 'X' || field.charAt(2) == 'X' &&
-                field.charAt(5) == 'X' && field.charAt(8) == 'X' || field.charAt(0) == 'X' && field.charAt(4) == 'X' &&
-                field.charAt(8) == 'X' || field.charAt(2) == 'X' && field.charAt(4) == 'X' && field.charAt(6) == 'X') {
+        if (line1.charAt(0) == 'X' && line1.charAt(1) == 'X' && line1.charAt(2) == 'X' || line2.charAt(0) == 'X' &&
+                line2.charAt(1) == 'X' && line2.charAt(2) == 'X' || line3.charAt(0) == 'X' && line3.charAt(1) == 'X' &&
+                line3.charAt(2) == 'X' || line1.charAt(0) == 'X' && line2.charAt(0) == 'X' && line3.charAt(0) == 'X' ||
+                line1.charAt(1) == 'X' && line2.charAt(1) == 'X' && line3.charAt(1) == 'X' || line1.charAt(2) == 'X' &&
+                line2.charAt(2) == 'X' && line3.charAt(2) == 'X' || line1.charAt(0) == 'X' && line2.charAt(1) == 'X' &&
+                line3.charAt(2) == 'X' || line1.charAt(2) == 'X' && line2.charAt(1) == 'X' && line3.charAt(0) == 'X') {
             xWin = true;
         }
 
+        if (line1.charAt(0) == 'O' && line1.charAt(1) == 'O' && line1.charAt(2) == 'O' || line2.charAt(0) == 'O' &&
+                line2.charAt(1) == 'O' && line2.charAt(2) == 'O' || line3.charAt(0) == 'O' && line3.charAt(1) == 'O' &&
+                line3.charAt(2) == 'O' || line1.charAt(0) == 'O' && line2.charAt(0) == 'O' && line3.charAt(0) == 'O' ||
+                line1.charAt(1) == 'O' && line2.charAt(1) == 'O' && line3.charAt(1) == 'O' || line1.charAt(2) == 'O' &&
+                line2.charAt(2) == 'O' && line3.charAt(2) == 'O' || line1.charAt(0) == 'O' && line2.charAt(1) == 'O' &&
+                line3.charAt(2) == 'O' || line1.charAt(2) == 'O' && line2.charAt(1) == 'O' && line3.charAt(0) == 'O') {
+            oWin = true;
+        }
         if (countMatches("X") - countMatches("O") > 1 || countMatches("O") -
                 countMatches("X")> 1) {
-            return "\nImpossible";
+            return "Impossible";
         } else if (xWin && oWin) {
-            return "\nImpossible";
+            return "Impossible";
         } else if (xWin) {
-            return "\nX is winner!";
+            return "X is winner!";
         } else if (oWin) {
-            return "\nO is winner!";
-        } else if (field.contains("_")) {
-            return "\nGame unfinished";
+            return "O is winner!";
+        } else if (line1.contains("_") || line2.contains("_") || line3.contains("_")) {
+            return "Game unfinished";
         } else {
-            return "\nDraw";
+            return "Draw";
         }
 
+    }
+    public boolean moveChecker (String coordinates) {
+        if (coordinates.length() != 3) {
+            System.out.println("You should enter two numbers separated by space!");
+            return false;
+        } else if (!(Character.isDigit(coordinates.charAt(0)) && Character.isDigit(coordinates.charAt(2)))) {
+            System.out.println("You should enter numbers");
+            return false;
+        } else if (!(Character.getNumericValue(coordinates.charAt(0)) <= 3
+                && Character.getNumericValue(coordinates.charAt(0)) <= 3)) {
+            System.out.println("Numbers should be from 1 to 3");
+            return false;
+        } else if (Character.getNumericValue(coordinates.charAt(0)) == 1) {
+            if (line1.charAt(Character.getNumericValue(coordinates.charAt(2)) - 1) != '_') {
+                System.out.println("This cell is occupied, choose another one");
+                return false;
+            } else {
+                return true;
+            }
+        } else if (Character.getNumericValue(coordinates.charAt(0)) == 2) {
+            if (line2.charAt(Character.getNumericValue(coordinates.charAt(2)) - 1) != '_') {
+                System.out.println("This cell is occupied, choose another one");
+                return false;
+            } else {
+                return true;
+            }
+        } else if (Character.getNumericValue(coordinates.charAt(0)) == 3) {
+            if (line3.charAt(Character.getNumericValue(coordinates.charAt(2)) - 1) != '_') {
+                System.out.println("This cell is occupied, choose another one");
+                return false;
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+    public void makeMove() {
+        System.out.println("Enter coordinates");
+        String coordinates = userInput.nextLine();
+
+        if (moveChecker(coordinates)) {
+            if (coordinates.charAt(0) == '1') {
+                line1 = replaceStringChar(line1, moveNow, Character.getNumericValue(coordinates.charAt(2)) - 1);
+            } else if (coordinates.charAt(0) == '2') {
+                line2 = replaceStringChar(line2, moveNow, Character.getNumericValue(coordinates.charAt(2)) - 1);
+            } else if (coordinates.charAt(0) == '3') {
+                line3 = replaceStringChar(line3, moveNow, Character.getNumericValue(coordinates.charAt(2)) - 1);
+            }
+        }
+    }
+
+    public String replaceStringChar(String string, char character, int index) {
+        return string.substring(0, index) + character + string.substring(index+1);
     }
 }
